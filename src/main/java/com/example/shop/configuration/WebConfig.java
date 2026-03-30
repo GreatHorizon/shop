0d7692094ab14b36a31ebd1d3b7b718532f6508a
Стас Гайсин <stas.gaisin@mail.ru>
2026-03-30 21:17:19 +0300

    feat: update base config for web flux

--- src/main/java/com/example/shop/configuration/WebConfig.java
+++ src/main/java/com/example/shop/configuration/WebConfig.java
@@ -1,14 +1,11 @@
 package com.example.shop.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebFluxConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
